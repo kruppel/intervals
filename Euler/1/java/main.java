7185efe76897231d30019eddd62d5450675897be
Kurt Ruppel <me@kurtruppel.com>
2015-10-07 14:41:31 -0700

    Fix indent to 2 for java

--- Euler/1/java/main.java
+++ Euler/1/java/main.java
@@ -1,14 +1,14 @@
 public class main {
   public static void main(String[] args) {
-      int arg = Integer.parseInt(args[0]);
-      int sum = 0;
+    int arg = Integer.parseInt(args[0]);
+    int sum = 0;
 
-      while (arg-- > 0) {
-        if (arg % 3 == 0 || arg % 5 == 0) {
-            sum += arg;
-        }
+    while (arg-- > 0) {
+      if (arg % 3 == 0 || arg % 5 == 0) {
+        sum += arg;
       }
+    }
 
-      System.out.println(sum);
+    System.out.println(sum);
   }
 }
